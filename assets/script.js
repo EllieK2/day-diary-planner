a76136bf630c709f062ffbb4e0d1a611e5010ae9
Elizabeth Kotwicka <elizabeth.kotwicka@hotmail.co.uk>
2021-10-17 13:09:26 +0100

    function to create the time with moment.js format

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
 console.log("hello"); 
+
+function displayTodaysDate() {
+    //display date on top of the page in a chosen specific format / use of moment.js  
+    currentDate.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+}
+
+// hours stored into array 
+const timeArray = []; 
+// loop through hours and push
+for (let i=9; i < 18; i++) 
+{
+    timeArray.push (
+        moment()
+            .hour(i)
+                .format('h, a')
+    );
+}
+console.log(timeArray);
